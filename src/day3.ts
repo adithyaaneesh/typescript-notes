b9703a81e85fbbbc7320827ac689ec1fbd5a94f4
adithyaaneesh <adithyaaneesh29@gmail.com>
2026-09-21 12:58:13 +0530

    minor changes

--- src/day3.ts
+++ src/day3.ts
@@ -11,84 +11,78 @@
 // }
 
 // In TS:
-// function add(a: number, b: number): number {
-//     return a + b;
-// }
-
+function add(a: number, b: number): number {
+    return a + b;
+}
 
 // Example:
-
-// function greet(name: string): string {
-//     return "Hello " + name;
-// }
-
-// console.log(greet("Miya"));
+function greet(name: string): string {
+    return "Hello " + name;
+}
+console.log(greet("Miya"));
 
 // 2. Function Without Parameters
 
-// function welcome(): string {
-//     return "Welcome to TypeScript";
-// }
-
-// console.log(welcome());
+function welcome(): string {
+    return "Welcome to TypeScript";
+}
+console.log(welcome());
 
 // 3. Function With Multiple Parameters
 
-// function product(a: number, b: number): number {
-//     return a * b;
-// }
-
-// console.log(add(10, 20));
+function product(a: number, b: number): number {
+    return a * b;
+}
+console.log(product(10, 20));
 
 // 4. Different Parameter Types
 
-// function studentInfo(name: string, age: number): string {
-//     return `${name} is ${age} years old`;
-// }
+function studentInfo(name: string, age: number): string {
+    return `${name} is ${age} years old`;
+}
 
-// console.log(studentInfo("Rahul", 22));
+console.log(studentInfo("Rahul", 22));
 
 // 5. Function Returning Boolean
 
-// function isAdult(age: number): boolean {
-//     return age >= 18;
-// }
-
-// console.log(isAdult(20));
+function isAdult(age: number): boolean {
+    return age >= 18;
+}
+console.log(isAdult(20));
 
 // 6. void Return Type
 
 // Sometimes a function doesn't return anything.
 
-// function printMessage(message: string): void {
-//     console.log(message);
-// }
+function printMessage(message: string): void {
+    console.log(message);
+}
 
-// printMessage("Hello TypeScript");
+printMessage("Hello TypeScript");
 
 // 7. Optional Parameters
 
 // Sometimes a parameter is optional. Use ?.
 
-// function greet(name: string, age?: number): string {
-//     if (age) {
-//         return `${name} is ${age} years old`;
-//     }else{
-//         return `Hello ${name}`;
-//     }
-// }
+function greeting(name: string, age?: number): string {
+    if (age) {
+        return `${name} is ${age} years old`;
+    }else{
+        return `Hello ${name}`;
+    }
+}
 
-// console.log(greet("Rahul"));
-// console.log(greet("Rahul", 22));
+console.log(greeting("Rahul"));
+console.log(greeting("Rahul", 22));
 
 // 8. Default Parameters
 
-// function greet(name: string = "Guest"): string {
-//     return `Hello ${name}`;
-// }
+function greetUser(name: string = "Guest"): string {
+    return `Hello ${name}`;
+}
 
-// console.log(greet());
-// console.log(greet("Miya"));
+console.log(greetUser());
+console.log(greetUser("Miya"));
 
 
 // 9. Optional vs Default Parameter 
@@ -106,11 +100,10 @@
 
 
 // 10. Function Expression
-
 // A function can also be stored inside a variable.
 
 // In JS:
-// const add = function(a, b) {
+// const add_num = function(a, b) {
 //     return a + b;
 // };
 
@@ -209,16 +202,47 @@
 //     return a + b;
 // };
 
+// console.log(add(2,3))
 
-// const add = (a: number, b: number): number => {
+// const addition = (a: number, b: number): number => {
 //     return a + b;
 // };
+// console.log(addition(2,3))
 
 // Both work.
 
 
 // 22. Practice Examples
 
-// 1. Create a function that accepts two numbers and returns their difference.
-// 2. Create a function that accepts a person's name and returns a greeting.
+// 1. Create a function returns the difference of 2 numbers.
+// 2. Create a function that pass a person's name and returns a greeting.
 // 3. Check whether a number is even.
+
+
+
+// 23.Workout
+
+// Create a function that returns their sum of 2 numbers.
+// Create a function that returns the square of a number.
+// Create a function pass a name and returns "Hello <name>".
+// Create a function that checks whether a number is positive.
+// Create a function that checks whether a person is eligible to vote.
+// Create a function that returns the largest of two numbers.
+// Create a function that converts Celsius to Fahrenheit.
+// Create a function that calculates the area of a rectangle.
+// Create a function that calculates the perimeter of a rectangle.
+// Create a function that pass a string and returns its length.
+
+// Optional/default parameters
+// Create a greet() function with an optional name.
+// Create a function with a default country value of "India".
+// Create a function that pass name and optional age.
+// Create a function that pass a default discount of 10.
+// Create a function that calculates price after discount.
+
+// Arrow functions
+// Create an arrow function to add two numbers.
+// Create an arrow function to multiply two numbers.
+// Create an arrow function to check whether a number is odd.
+// Create an arrow function to convert a string to uppercase.
+// Create an arrow function to check whether a student passed.
